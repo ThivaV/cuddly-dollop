ee148f2fc7fa5defa86a6519224099f440889161
Thivaharan V <thivaharan28@gmail.com>
2022-02-22 23:49:43 +0530

    amazon

--- Amazon/OA2021_FreshPromotion.cs
+++ Amazon/OA2021_FreshPromotion.cs
@@ -11,23 +11,41 @@ namespace Amazon
         public int freshPromotion(List<List<string>> codeList, List<string> shoppingCart)
         {
             int cartIndex = 0;
-            foreach (var codes in codeList)
+            int codeIndex = 0;
+
+            while (cartIndex < shoppingCart.Count && codeIndex < codeList.Count)
             {
-                bool result = hasOrder(shoppingCart, cartIndex, codes);
+                string currentItem = shoppingCart[cartIndex];
+
+                if (((codeList[codeIndex][0].Equals("anything")) || codeList[codeIndex][0].Equals(currentItem)) &&
+                    hasOrder(shoppingCart, cartIndex, codeList[codeIndex])) 
+                {
+                    cartIndex += codeList[codeIndex++].Count;
+                }
+                else
+                {
+                    cartIndex++;
+                }
             }
+
+            return codeIndex == codeList.Count ? 1 : 0;
         }
 
         private bool hasOrder(List<string> shoppingCart, int cartIndex, List<string> codes)
         {
-            int start = 0;
-            int end = 0;
-            for (int i = 0; i < codes.Count; i++)
+            foreach (var code in codes)
             {
-                if ((shoppingCart[cartIndex] == codes[i]) || (shoppingCart[cartIndex] == "anything"))
+                if (cartIndex < shoppingCart.Count && (code.Equals("anything") || shoppingCart[cartIndex].Equals(code))) 
                 {
-
+                    cartIndex++;
+                }
+                else 
+                {
+                    return false;
                 }
             }
+
+            return true;
         }
     }
 }
