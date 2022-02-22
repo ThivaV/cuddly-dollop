5ed3d30833bc3490968ef6d57fbb67f980b15762
Thivaharan V <thivaharan28@gmail.com>
2022-02-22 23:14:17 +0530

    amazon

---
+++ Amazon/OA2021_FreshPromotion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazon
+{
+    internal class OA2021_FreshPromotion
+    {
+        public int freshPromotion(List<List<string>> codeList, List<string> shoppingCart)
+        {
+            int cartIndex = 0;
+            foreach (var codes in codeList)
+            {
+                bool result = hasOrder(shoppingCart, cartIndex, codes);
+            }
+        }
+
+        private bool hasOrder(List<string> shoppingCart, int cartIndex, List<string> codes)
+        {
+            int start = 0;
+            int end = 0;
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if ((shoppingCart[cartIndex] == codes[i]) || (shoppingCart[cartIndex] == "anything"))
+                {
+
+                }
+            }
+        }
+    }
+}
